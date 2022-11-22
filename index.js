49c1d568fbffdc60e7040acc0dadef0f91b2a26d
kLeitich <kevleitich@gmail.com>
2022-11-22 12:15:45 +0300

    adding brand color to social media logos

---
+++ index.js
@@ -0,0 +1,10 @@
+// alert("subscribe to our newsletter")
+$(document).ready(function() {
+    $(".dropdown, .btn-group").hover(function() {
+        var dropdownMenu = 
+            $(this).children(".dropdown-menu");
+        if (dropdownMenu.is(":visible")) {
+            dropdownMenu.parent().toggleClass("open");
+        }
+    });
+});
